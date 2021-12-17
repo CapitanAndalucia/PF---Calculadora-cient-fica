eb49fd712a94dee8f34becb3dbeee3bc57a7fa83
Fernando de la Rosa Moreno <delaehijo@gmail.com>
2021-12-17 14:23:18 +0100

    Clase calculadora creada

---
+++ src/CalculadoraCientifica.java
@@ -0,0 +1,31 @@
+
+public class CalculadoraCientifica {
+/**
+ * 1. Implementar una calculadora científica:
+- Operaciones aritméticas básicas
+- Potencias
+- Cálculo del resto
+- seno, coseno y tangente
+- logaritmos.
+
+2. Controlar los posibles errores
+3. Documentar con JavaDoc
+
+
+Se espera:
+
+    Que el menú esté una función
+    Que el programa continúe hasta que el usuario decida salir.
+    Que se creen y lancen errores
+    Que se controlen errores que se puedan dar por otras clases.
+    Dividir al máximo en funciones
+    Se espera un control fin de errores
+    Un main muy estructurado y legible.
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
